fd3055cfba25509319b1c53b8b42c9b4bd0b713b
jaacker25 <jaguilar.ice@gmail.com>
2020-06-02 15:51:45 -0500

    Base del proyecto creada e implementación de vista Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,84 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-link to="/weather" class="logoHomeBackLayer">
+
+      <div class="logoHome">
+        <h1>G&nbsp;E&nbsp;T&nbsp;&nbsp;I&nbsp;N</h1>
+      </div>
+
+    </router-link>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
+  name: "Home"
 };
 </script>
+
+<style scoped>
+.home {
+  background: linear-gradient(
+    310deg,
+    rgb(50, 36, 129) 0%,
+    rgba(25, 50, 255, 0.8) 40%,
+    rgba(70, 80, 255, 0.5) 100%
+  );
+  background-size: cover;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.logoHomeBackLayer {
+  width: 300px;
+  height: 300px;
+  background: #5885f888;
+
+  border: solid 2px rgba(255, 255, 255, 0.5);
+  border-radius: 50%;
+  box-shadow:black 5px 5px 100px;
+  transition: 0.3s ease-in;
+  text-decoration: none;
+}
+.logoHomeBackLayer:hover{
+  border: solid 2px #f1b434;
+  box-shadow:black 5px 5px 90px;
+}
+
+.logoHomeBackLayer:hover h1{
+  color: #f1b434;
+}
+
+.logoHome {
+  background-image: url("../assets/logo.png");
+  background-size: contain;
+  opacity: 100%;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: 0.2s ease-in;
+}
+
+h1 {
+  color: white;
+  opacity: 75%;
+  font-size: 1.5rem;
+  font-weight: 800;
+
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  cursor: pointer;
+  transition: 0.2s ease-in;
+}
+</style>
---
+++ src/views/Weather.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="weather">
+    <div id="nav">
+      <router-link to="/">Return</router-link>
+    </div>
+    <h1>Here we have the weather items</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Weather',
+  data(){
+    return{
+      base_url: 'http://api.openweathermap.org/data/2.5/',
+      api_key: process.env.VUE_APP_KEY_APPI
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
